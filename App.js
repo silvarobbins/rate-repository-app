c1eff32b0b3c84e3d268344b214980a1c3682714
Silva Robbins <silva.robbins@gmail.com>
2022-04-07 21:55:00 +0300

    10.2

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("hi")
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app</Text>
+      <Text>TEST</Text>
       <StatusBar style="auto" />
     </View>
   );
